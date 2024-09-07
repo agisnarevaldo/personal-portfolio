ccdc76835475b2677d980ca683b5544592912f47
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-09-07 20:11:02 +0700

    feat: Add page component for projects
    This commit adds a new page component for displaying projects. The component includes a card component for each project and showcases the developer's works on frontend development. This feature was implemented based on recent user commits and repository commits.

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,24 @@
+import Card from "@/app/components/card";
+
+export default function Page () {
+    return (
+        <div className="px-4 lg:px-12">
+            <div
+                className="text-3xl font-semibold tracking-wide relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
+                <div
+                    className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-2 from-indigo-500 via-blue-500 to-blue-900 [text-shadow:0_0_rgba(0,0,0,0.1)]">
+                    <span className="">Projects</span>
+                </div>
+                <div
+                    className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-indigo-500 via-blue-500 to-blue-900 py-2">
+                    <span className="">Projects</span>
+                </div>
+            </div>
+            <p>Showcase of my works on frontend development.</p>
+
+            <div>
+                <Card />
+            </div>
+        </div>
+    )
+}
