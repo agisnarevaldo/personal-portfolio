5109be0ae04b0272f1c3f9c65a3f22df939665d4
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-09-26 22:55:38 +0700

    feat: Add About page component
    This commit adds a new About page component, which displays information about the frontend developer. The component includes a Heading component for the title and a paragraph for the description. This feature was implemented based on recent user commits and repository commits.

---
+++ src/app/components/about.tsx
@@ -0,0 +1,10 @@
+import Heading from "@/app/components/heading";
+
+export default function About () {
+    return (
+        <div className="px-4 lg:px-12 mb-2" id="about">
+            <Heading title="About" />
+            <p>Hi, I&apos;m a frontend developer. I love to create a beautiful and responsive website.</p>
+        </div>
+    )
+}
