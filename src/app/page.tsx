1a6a2db96a6af4afa46d20feaf1ac4a048ef0bdb
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-09-26 22:55:55 +0700

    refactor: Update home page layout and add Projects and About components
    This commit refactors the home page layout by updating the styling and removing unused code. Additionally, it adds the Projects and About components to the home page, allowing users to view and navigate to project details and information about the frontend developer. This feature was implemented based on recent user commits and repository commits.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import {BackgroundBeamsWithCollision} from "@/app/components/ui/background-beams-with-collision";
 import Card from "@/app/components/card";
+import Projects from "@/app/components/projects";
+import About from "@/app/components/about";
 // import Navbar from "@/app/components/navbar";
 
 export default function Home() {
@@ -7,55 +9,42 @@ export default function Home() {
         <>
             <BackgroundBeamsWithCollision>
                 {/*<Navbar/>*/}
-                <h2 className="text-3xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center   text-white font-sans tracking-tight">
-                    Who Am I ?{" "}
-                    <div
-                        className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
+                <div>
+                    <h2 className="text-3xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center   text-white font-sans tracking-tight">
+                        Who am I ?{" "}
                         <div
-                            className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-indigo-500 via-blue-500 to-green-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
-                            <span className="">Agisna Revaldo.</span>
+                            className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
+                            <div
+                                className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-indigo-500 via-blue-500 to-green-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
+                                <span className="">Agisna Revaldo.</span>
+                            </div>
+                            <div
+                                className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-indigo-500 via-blue-500 to-green-500 py-4">
+                                <span className="">Agisna Revaldo.</span>
+                            </div>
                         </div>
+                    </h2>
+                    <h2 className="text-3xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center   text-white font-sans tracking-tight">
+                        What am I doing?{" "}
                         <div
-                            className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-indigo-500 via-blue-500 to-green-500 py-4">
-                            <span className="">Agisna Revaldo.</span>
+                            className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
+                            <div
+                                className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-indigo-500 via-blue-500 to-green-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
+                                <span className="">Learn Coding by Doing Something.</span>
+                            </div>
+                            <div
+                                className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-indigo-500 via-blue-500 to-green-500 py-4">
+                                <span className="">Learn Coding by Doing Something.</span>
+                            </div>
                         </div>
-                    </div>
-                </h2>
+                    </h2>
+                </div>
 
             </BackgroundBeamsWithCollision>
 
-            <div className="px-4 lg:px-12" id="project">
-                <div
-                    className="text-3xl font-semibold tracking-wide relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
-                    <div
-                        className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-2 from-indigo-500 via-blue-500 to-blue-900 [text-shadow:0_0_rgba(0,0,0,0.1)]">
-                        <span className="">Projects</span>
-                    </div>
-                    <div
-                        className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-indigo-500 via-blue-500 to-blue-900 py-2">
-                        <span className="">Projects</span>
-                    </div>
-                </div>
-                <p>Showcase of my works on frontend development.</p>
+            <Projects />
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                    <Card
-                        title="Desa Dirgahayu"
-                        description="Web Profile Desa Dirgahayu, sebuah desa yang terletak di Kecamatan Kadipaten, Kabupaten Tasikmalaya."
-                        image="/desadirgahayu.png"
-                    />
-                    <Card
-                        title="Desa Dirgahayu"
-                        description="Web Profile Desa Dirgahayu, sebuah desa yang terletak di Kecamatan Kadipaten, Kabupaten Tasikmalaya."
-                        image="/desadirgahayu.png"
-                    />
-                    <Card
-                        title="Desa Dirgahayu"
-                        description="Web Profile Desa Dirgahayu, sebuah desa yang terletak di Kecamatan Kadipaten, Kabupaten Tasikmalaya."
-                        image="/desadirgahayu.png"
-                    />
-                </div>
-            </div>
+            <About />
         </>
     );
 }
